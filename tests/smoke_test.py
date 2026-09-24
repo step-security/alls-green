8f3ce0303c3a1f247246b1b88ed7d41c0b05a55a
Raj-StepSecurity <rajkumar@stepsecurity.io>
2026-09-24 11:16:44 +0530

    package-lock updated

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,13 +1,5 @@
 """Initial test infra smoke tests."""
 
-import normalize_needed_jobs_status  # pth via editable install in tox
-
-
-def test_smoke() -> None:
-    """Check that the imported module is truthy."""
-    assert normalize_needed_jobs_status
-"""Initial test infra smoke tests."""
-
 import json
 import pathlib
 import sys
